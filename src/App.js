621c0d914501f77c062ff747017edc89f20e671c
ARYANREDDY2005 <2211cs010559@mallareddyuniversity.ac.in>
2025-02-04 00:14:40 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  let username = "Aryan";
+  return (
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>Aryan</h1>
+    //   </header>
+    // </div>
+    // <section>
+    //   <h1>One Jsx element</h1>
+    //   <p>lorem1</p>
+    //   <p>lorem2</p>
+    // </section>
+    <div>
+    <h1>{username}</h1>
+    <p>{100+100}</p>
+    </div>
+  );
+}
+
+export default App;
